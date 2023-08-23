0b5c05e202a053d08a7250be7dfd6239d0a13dfb
hmithapara <hmithapara@horizontal.com>
2023-08-23 12:16:02 +0530

    changes in breadcrumb component

--- src/components/Foundation/Breadcrumb/Breadcrumb.tsx
+++ src/components/Foundation/Breadcrumb/Breadcrumb.tsx
@@ -35,41 +35,43 @@ export interface BreadcrumbProps {
 
 export const Default = ({ fields }: BreadcrumbProps): JSX.Element | false => {
   return (
-    fields?.data?.item?.field?.Title.value != 'Home' && (
-      <div className="container mx-auto mt-5">
-        <ul className="flex flex-row [&>li]:text-3xl uppercase">
-          {fields?.data?.item?.ancestors?.reverse().map((item, index: number) => {
-            if (Object.keys(item).length > 0) {
-              return (
-                <li key={index} className="after:content-['/'] after:mx-2">
-                  <Link
-                    href={item?.path?.replace(
-                      `${fields?.data?.rootPath?.siteInfo?.rootPath}/Home`,
-                      ''
-                    )}
-                    className="hover:text-blue-500 transition-all"
-                  >
-                    {<Text field={item?.field?.Title} />}
-                  </Link>
-                </li>
-              );
-            }
-          })}
-          <li>
-            {
-              <Link
-                href={fields?.data?.item?.path.replace(
-                  `${fields?.data?.rootPath?.siteInfo?.rootPath}/Home`,
-                  ''
-                )}
-                className="pointer-events-none font-bold"
-              >
-                <Text field={fields?.data?.item?.field?.Title} />
-              </Link>
-            }
-          </li>
-        </ul>
-      </div>
-    )
+    <div
+      className={`container mx-auto mt-5 ${
+        fields?.data?.item?.field?.Title.value != 'Home' ? 'hidden' : ''
+      }`}
+    >
+      <ul className="flex flex-row uppercase [&>li]:text-3xl">
+        {fields?.data?.item?.ancestors?.reverse().map((item, index: number) => {
+          if (Object.keys(item).length > 0) {
+            return (
+              <li key={index} className="after:mx-2 after:content-['/']">
+                <Link
+                  href={item?.path?.replace(
+                    `${fields?.data?.rootPath?.siteInfo?.rootPath}/Home`,
+                    ''
+                  )}
+                  className="transition-all hover:text-blue-500"
+                >
+                  {<Text field={item?.field?.Title} />}
+                </Link>
+              </li>
+            );
+          }
+        })}
+        <li>
+          {
+            <Link
+              href={fields?.data?.item?.path.replace(
+                `${fields?.data?.rootPath?.siteInfo?.rootPath}/Home`,
+                ''
+              )}
+              className="pointer-events-none font-bold"
+            >
+              <Text field={fields?.data?.item?.field?.Title} />
+            </Link>
+          }
+        </li>
+      </ul>
+    </div>
   );
 };
