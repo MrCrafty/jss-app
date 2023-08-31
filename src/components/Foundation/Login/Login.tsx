3125d0a73c62d0a37ec3b543a9be0a39c4565e12
hmithapara <hmithapara@horizontal.com>
2023-08-31 15:47:07 +0530

    removed unwanted imports

--- src/components/Foundation/Login/Login.tsx
+++ src/components/Foundation/Login/Login.tsx
@@ -2,7 +2,7 @@
 import React, { FormEvent, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import axios from 'axios';
+// import axios from 'axios';
 
 export const Default = (): JSX.Element => {
   const router = useRouter();
