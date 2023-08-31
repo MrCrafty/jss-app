69cdb896fe4d7d0834a85f1c335589447b55ae73
hmithapara <hmithapara@horizontal.com>
2023-08-31 15:34:50 +0530

    changes in next config

--- next.config.js
+++ next.config.js
@@ -21,7 +21,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  crossOrigin: 'anonymous',
 
   i18n: {
     // These are all the locales you want to support in your application.
@@ -61,6 +60,24 @@ const nextConfig = {
       {
         source: '/api/auth/:path*',
         destination: `${jssConfig.sitecoreApiHost}/api/auth/:path*`,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ];
   },
